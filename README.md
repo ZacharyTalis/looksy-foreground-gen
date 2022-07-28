0a3e1a3ea31f584651494b75a40dbfd1e60442d3
u-ndefine <41176671+u-ndefine@users.noreply.github.com>
2022-07-28 22:22:27 +0800

    Add color overlaying, and more documentation

--- README.md
+++ README.md
@@ -11,15 +11,55 @@ Thanks to Mak for the foreground image size equation!
 ## Run info
 
 ### Format
+
 ```shell
-python3 main.py <icons> <x,y> <placements>
+python3 main.py <icons> <colors> <x,y> <placements>
 ```
 
-### Example
-To generate a 2x4 foreground image with row order `blanks -> stars -> hearts -> blanks`
-```shell
-python3 main.py star,heart 2,4 0,0-1,1-2,2
-```
+- **Icons**: Names of icons to be used, which can be found in `icons` folder. Each icon is separated by a comma `,`.
+- **Colors**: Names of colors to be used, which can be found in the [Colors section](#colors). Each color is separated by a comma `,`. The first color will be used by icons by default.
+- **x,y**: The panel's width and height respectively.
+- **Placements**: Syntax to place the icons:
+  - A number specifies the index of icons to be used. Using the number `0` is a blank icon.
+  - The current cell is in the first row and column by default. The character `,` moves the cell to the next column. `'` moves it to the next row and goes back to the first column.
+  - By adding `c` just before the number, the color can be specified, by adding a number next to it indicating its index. If not used, it defaults to the first color specified.
 
 ### Paths
+
 All icons should be PNGs and reside within the `icons` folder. Each output is saved to the `output` folder as `foreground.png`.
+
+### Colors
+
+Available predefined colors currently are:
+
+- Looksy: (available by adding `looksy-` into the beginning, then specifying the color at the below table)
+
+| Colors      | Colors        | Colors       |
+| ----------- | ------------- | ------------ |
+| `white`     | `black`       | `grey`       |
+| `red`       | `pink`        | `darkred`    |
+| `orange`    | `lightred`    | `orangered`  |
+| `yellow`    | `lightyellow` | `gold`       |
+| `darkgreen` | `green`       | `lightgreen` |
+| `blue`      | `blueviolet`  | `lightcyan`  |
+| `purple`    | `violet`      | `magenta`    |
+
+- Other options are: `black`, `grey`, `white`, `darkred`, `red`, `orange`, `olive`, `yellow`, `darkgreen`, `green`, `teal`, `cyan`, `darkblue`, `blue`, `purple`, and `magenta`.
+
+- The program supports hexadecimal format, either normal `#008cff` or shorthand `#08f`.
+
+- There is also the RGB format `red-green-blue`, where each channel has a range between 0 to 255 inclusively (e.g. `0-140-255`).
+
+### Examples
+
+To generate a 2x4 foreground image with row order `blanks -> stars -> hearts -> blanks`:
+
+```shell
+python3 main.py star,heart white 2,4 0,0-1,1-2,2
+```
+
+A 3x3 foreground image with hearts placed diagonally, and each row is colored in red, green, then blue:
+
+```shell
+python3 main.py heart red,green,blue 3,3 0,0,1-0,1c1,0-1c2
+```
--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from PIL import Image
 # If help requested, print help message
 if ((len(sys.argv) == 1) or ("help" in sys.argv[1])):
     helpMessage = \
-"""Format: python3 main.py <icons> <x,y> <placements>
+"""Format: python3 main.py <icons> <colors> <x,y> <placements>
 
 Example: To generate a 2x4 foreground image with row order \"blanks -> stars -> hearts -> blanks\"
-python3 main.py star,heart 2,4 0,0-1,1-2,2
+python3 main.py star,heart white 2,4 0,0-1,1-2,2
 
 Paths: All icons should be PNGs and reside within the \"icons\" folder. Each output is saved to the \"output\" folder as \"foreground.png\"."""
     print(helpMessage)
@@ -29,9 +29,10 @@ gapSizePx  = 82 # cellSizePx + 24px between cells
 # Constants (based on arguments)
 try:
     iconNames     = sys.argv[1].split(",")
-    rawDimensions = [int(x) for x in sys.argv[2].split(",")]
+    colorNames    = sys.argv[2].split(",")
+    rawDimensions = [int(x) for x in sys.argv[3].split(",")]
     dimensions    = [calcImageSize(x) for x in rawDimensions]
-    placements    = [x.split(",") for x in sys.argv[3].split("-")]
+    placements    = [[c.split('c') for c in x.split(",")] for x in sys.argv[4].split("-")]
 except:
     sys.exit("Arguments missing or not recognized! Refer to \"main.py --help\" for argument format.")
 
@@ -60,7 +61,7 @@ if (len(failedRows) > 0):
 icons = {}
 for iconName in iconNames:
     try:
-        icons[iconName] = Image.open(f"{iconDir}{iconName}.png")
+        icons[iconName] = Image.open(f"{iconDir}{iconName}.png").convert("RGBA")
     except:
         sys.exit(f"{iconName}.png not found in icons folder!")
     try:
@@ -68,20 +69,103 @@ for iconName in iconNames:
     except:
         sys.exit("Image resize failed!")
 
+# Generate color dictionary
+colors = \
+    {
+      # Colors from Looksy
+      "looksy-black": (0, 0, 0),
+      "looksy-white": (255, 255, 255), 
+      "looksy-lightgray": (204, 204, 204), 
+
+      "looksy-red": (255, 0, 0),
+      "looksy-pink": (255, 102, 179),
+      "looksy-darkred": (128, 0, 0),
+
+      "looksy-orange": (255, 165, 0),
+      "looksy-lightred": (255, 102, 102),
+      "looksy-orangered": (255, 64, 0),
+
+      "looksy-yellow": (255, 255, 0),
+      "looksy-lightyellow": (255, 255, 128),
+      "looksy-gold": (255, 201, 0),
+
+      "looksy-darkgreen": (0, 128, 0),
+      "looksy-green": (0, 255, 0),
+      "looksy-lightgreen": (176, 255, 176),
+
+      "looksy-blue": (0, 0, 255),
+      "looksy-blueviolet": (104, 103, 253),
+      "looksy-lightcyan": (128, 255, 255),
+
+      "looksy-purple": (128, 0, 128),
+      "looksy-violet": (129, 1, 255),
+      "looksy-magenta": (255, 7, 255),
+      
+      # Others
+      "black": (0, 0, 0),
+      "grey": (128, 128, 128),
+      "white": (255, 255, 255),
+      "darkred": (128, 0, 0),
+      "red": (255, 0, 0),
+      "orange": (255, 165, 0),
+      "olive": (128, 128, 0),
+      "yellow": (255, 255, 0),
+      "darkgreen": (0, 128, 0),
+      "green": (0, 255, 0),
+      "teal": (0, 128, 128),
+      "cyan": (0, 255, 255),
+      "darkblue": (0, 0, 128),
+      "blue": (0, 0, 255),
+      "purple": (128, 0, 128),
+      "magenta": (255, 0, 255)
+    }
+
+for colorName in colorNames:
+    # using predefined table
+    if colorName in colors:
+        pass
+
+    # hexcode (3 digit)
+    elif len(colorName) == 4 and colorName[0] == '#' and all(x in "0123456789ABCDEFabcdef" for x in colorName[1:]):
+        colors[colorName] = (int(x, 16) * 16 for x in colorName[1:])
+
+    # hexcode (6 digit)
+    elif len(colorName) == 7 and colorName[0] == '#' and all(x in "0123456789ABCDEFabcdef" for x in colorName[1:]):
+        colors[colorName] = tuple(int(colorName[r:r+2], 16) for r in range(1, 7, 2))
+
+    # rgb 
+    elif len(colorName.split('-')) == 3:
+        colors[colorName] = (int(x) for x in colorName.split('-'))
+
+    else:
+        sys.exit("Syntax error! {colorName}")
+
 # Generate image
 baseImage = Image.new("RGBA", dimensions)
 iconPlaced = False
 currentY = edgeSizePx
 for row in placements:
     currentX = edgeSizePx
-    for iconValue in row:
+    for cell in row:
+        iconValue = cell[0]
         if (iconValue.isdigit()):
             iconValue = int(iconValue)
             if (iconValue > len(iconNames)):
                 sys.exit(f"Specified icon value \"{iconValue}\" doesn't correspond to any specified icon!")
             if (iconValue > 0):
-                baseImage.paste(icons[iconNames[iconValue - 1]], (currentX, currentY))
-                iconPlaced = True
+                colorValue = cell[1] if len(cell) > 1 and cell[1] else '0'
+                if(colorValue.isdigit()):
+                    colorValue = int(colorValue)
+                    if (colorValue >= len(colorNames)):
+                        sys.exit(f"Specified color value \"{colorValue}\" doesn't correspond to any specified color icon!")
+
+                    currentIcon = icons[iconNames[iconValue - 1]]
+                    rgb = (cIcon.point(lambda x: x * cColor / 255) for cIcon, cColor in zip(currentIcon.split(), colors[colorNames[colorValue]]))
+                    a = currentIcon.split()[-1]
+                    baseImage.paste(Image.merge("RGBA", [*rgb, a]), (currentX, currentY))
+                    iconPlaced = True
+                else:
+                    sys.exit(f"Color value {colorValue} isn't a digit!")
         elif (not len(iconValue) == 0):
             sys.exit(f"Icon value {iconValue} isn't a digit!")
         currentX += gapSizePx
